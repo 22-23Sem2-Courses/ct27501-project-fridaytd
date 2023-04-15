49a7e772a739304aafd25ced3ef6645d42c317e9
fridaytd <friday.infortech@gmail.com>
2023-04-15 19:12:01 +0700

    update

--- src/Controllers/Cafe.php
+++ src/Controllers/Cafe.php
@@ -6,8 +6,17 @@ class Cafe extends \MVC\Core\Controller
 {
     function Show()
     {
+        $model = $this->model('Product');
+        $products = [];
+        foreach ($model->getAllProducts() as $product) {
+            $product = $product->getProductInfor();
+            if ($product['type'] === 'Coffee') {
+                $products[] = $product;
+            }
+        }
         $this->view('template', [
-            'page' => 'CafePage'
+            'page' => 'Cafe',
+            'products' => $products
         ]);
     }
 }
--- src/Views/user.php
+++ src/Views/user.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" media="all" href="<?= '/css/style.css' ?>"> -->
+
 
     <style>
         .bd-placeholder-img {
@@ -70,8 +70,6 @@
     </style>
 
 
-    <!-- Custom styles for this template -->
-    <link href="/css/dashboard.css" rel="stylesheet">
 </head>
 
 <body>
